ea8ee6689da04b50933daa2155d017e777062239
HWONH <95hyun408@naver.com>
2022-03-28 19:49:58 +0900

    global & local variable

--- js/script.js
+++ js/script.js
@@ -64,3 +64,13 @@ console.log($question_num) // true
 // eval() 내장함수;결합할 수 없는 요소들을 강제 결합(문자형 데이터를 갖고 있는 숫자 예시 - "1000")
 var $eval=eval("1000*2000");
 console.log($eval); // 2000000
+/* ---------------------------------------------------- */
+var $num=400; // 전역(global)변수;함수문 내외부에 모두 영향을 미칠 수 있는 변수
+function calc_01(){ // 2. 함수문 실행
+    var $num=300; // 3. 변수에 대한 정의/지역(local) 변수;함수문 내부에서만 사용할 수 있음
+    return $num; // 4. 지역변수에 대한 데이터 값을 돌려준다 => calc_01() 최초의 호출문 값 전달
+}
+calc_01();
+console.log($num); // 400
+/* var final_num=calc_01(); // 1. 함수문 호출 5. final_num = return문에 의해 돌려받은 데이터 값을 최종 전역 변수에 저장
+document.write(final_num, "<br>"); // 300
